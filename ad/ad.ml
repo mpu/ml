e085906c143755f1b0b37b7cad4a5e91fdf334c4
Quentin Carbonneaux <quentin@c9x.me>
2019-11-13 21:19:38 +0100

    fwd mode autograd

--- ad/ad.ml
+++ ad/ad.ml
@@ -8,6 +8,23 @@ module Fresh = struct
       !cnt
 end
 
+let pp_w fmt () =
+  Format.fprintf fmt "@ "
+
+let rec pp_l_ pp_s pp fmt seq =
+  match seq () with
+  | Seq.Nil -> ()
+  | Seq.Cons (x, l)
+    when l () = Seq.Nil ->
+    pp fmt x
+  | Seq.Cons (x, l) ->
+    Format.fprintf fmt "%a%a%a"
+      pp x pp_s () (pp_l_ pp_s pp) l
+
+let pp_l pp = pp_l_ pp_w pp
+
+let pp_ls = pp_l Format.pp_print_string
+
 module Stat = struct
   type t = (string, int) Hashtbl.t
 
@@ -41,7 +58,6 @@ module CG = struct
     | Par of string
     | Con of float
     | Add of node * node
-    | Sub of node * node
     | Mul of node * node
 
   let st = Stat.mk ()
@@ -61,9 +77,6 @@ module CG = struct
     | Add (n1, n2) ->
       Stat.bump st "add";
       fix n1 +. fix n2
-    | Sub (n1, n2) ->
-      Stat.bump st "sub";
-      fix n1 -. fix n2
     | Mul (n1, n2) ->
       Stat.bump st "mul";
       fix n1 *. fix n2
@@ -86,6 +99,52 @@ module CG = struct
     in
     fix n
 
+  (* extend the computation graph
+     to compute the differential
+     of the argument node and
+     return it *)
+  let fwd_grad n =
+    let rec go n =
+      match n.op with
+      | Con _ ->
+        SMap.empty
+      | Par x ->
+        let x' = x^"'" in
+        SMap.singleton x' (mk (Par x'))
+      | Add (n1, n2) ->
+        let comb _ o1 o2 =
+          match o1,o2 with
+          | Some d1, Some d2 ->
+            Some (mk (Add (d1, d2)))
+          | Some d, None
+          | None, Some d ->
+            Some d
+          | None, None ->
+            None
+        in
+        SMap.merge comb
+          (go n1)
+          (go n2)
+      | Mul (n1, n2) ->
+        let comb _ o1 o2 =
+          match o1,o2 with
+          | Some d1, Some d2 ->
+            let m1 = mk (Mul (d1, n2)) in
+            let m2 = mk (Mul (n1, d2)) in
+            Some (mk (Add (m1, m2)))
+          | Some d1, None ->
+            Some (mk (Mul (d1, n2)))
+          | None, Some d2 ->
+            Some (mk (Mul (n1, d2)))
+          | None, None ->
+            None
+        in
+        SMap.merge comb
+          (go n1)
+          (go n2)
+    in
+    go n
+
 end
 
 let _ =
@@ -96,15 +155,38 @@ let _ =
     let x3  = mk (Mul (x2, x)) in
     let tx3 = mk (Mul (mk (Con 2.), x3)) in
     let y   = mk (Add (tx3, x2)) in
+    (* 2 x^3 + x^2 *)
     y
   in
+  let pp_e fmt e =
+    let pp_bnd fmt (a, b) =
+      Format.fprintf fmt "%s=%g" a b
+    in
+    Format.fprintf fmt "@[<h>%a@]"
+      (pp_l pp_bnd) (SMap.to_seq e)
+  in
+  let epf = Format.eprintf in
   let env = SMap.singleton "x" 3. in
-  Format.eprintf "g1(x=3) = %f@."
-    (CG.eval env g1);
-  Format.eprintf "Stats, no memoization: %a@."
+
+  epf "g1(%a) = %f@."
+    pp_e env (CG.eval env g1);
+  epf "Stats, no memoization: %a@."
     Stat.dump CG.st;
   Stat.reset CG.st;
-  Format.eprintf "g1(x=3) = %f@."
-    (CG.evalm env g1);
-  Format.eprintf "Stats, with memoization: %a@."
-    Stat.dump CG.st
+
+  epf "g1(%a) = %f@."
+    pp_e env (CG.evalm env g1);
+  epf "Stats, with memoization: %a@."
+    Stat.dump CG.st;
+  Stat.reset CG.st;
+
+  let dg1 = CG.fwd_grad g1 in
+  epf "Grad keys: @[<h>%a@]@."
+    pp_ls (Seq.map fst @@ SMap.to_seq dg1);
+  
+  let env = SMap.of_seq @@
+    List.to_seq ["x", 2.; "x'", 1.] in
+  let dg1x = SMap.find "x'" dg1 in
+  epf "dg1(%a) = %f@."
+    pp_e env (CG.evalm env dg1x)
+
